6db22e22fd66d9fec8e590ad5dc4153ee1d23634
Urban Sanden <hej@urre.me>
2016-08-17 20:46:57 +0200

    Dont hotlink css

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const autoprefixer = require('autoprefixer');
 
 const common = {
   entry: [
+    './node_modules/basscss/css/basscss.css',
     './src/css/main.css',
     './src/css/type.css',
     './src/css/media.css',
