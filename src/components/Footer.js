cddf722cac636cbefd3a20b193cf42d0c5ba9136
Urban Sanden <hej@urre.me>
2016-08-25 21:33:05 +0200

    Change footer copy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
 	return (
 			<div className="sm-flex flex-wrap mxn2">
 				<div className="sm-col-12 p2 center">
-					<p><small>Made by <a href="https://urre.me">Urban Sandén</a></small></p>
+					<p><small><a href="https://twitter.com/urre">Suggest a concert</a></small></p>
 			</div>
 			</div>
 	);
