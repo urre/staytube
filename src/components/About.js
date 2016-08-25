83dc94d461db88895a26affe68e62a712f5a7fa5
Urban Sanden <hej@urre.me>
2016-08-25 21:32:57 +0200

    Add a list of tools

--- src/components/About.js
+++ src/components/About.js
@@ -3,14 +3,23 @@ import Helmet from 'react-helmet';
 
 class About extends React.Component {
 	render() {
-		return (<div className="sm-flex flex-wrap">
-			<div className="sm-col-12 p2">
+		return (<div className="sm-flex flex-wrap mxn2">
+			<div className="col col-6 mx-auto p2">
 				<Helmet
   			title="About | Staytube"
   			meta={[{ property: 'og:title', content: 'About' }]}
 				/>
 				<h1>About</h1>
-				<p>Dedicated to great full length concerts on YouTube.</p>
+				<p>Dedicated to great full length concerts. Exploring the concept of using "Youtube as a CMS". Made by <a href="https://twitter.com/urre">Urban Sandén (@urre)</a></p>
+				<h3>Tools</h3>
+				<ul className="list-reset tools">
+					<li>ReactJS</li>
+					<li>Babel</li>
+					<li>Webpack</li>
+					<li>PostCSS</li>
+					<li>Basscss</li>
+					<li>YouTube API</li>
+				</ul>
 				</div>
 			</div>);
 	}
