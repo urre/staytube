0e78fbb8f4a6bfc5ddba7c769c85b05a15b98597
Urban Sanden <hej@urre.me>
2016-08-26 08:52:40 +0200

    Added tools

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import Helmet from 'react-helmet';
 class About extends React.Component {
 	render() {
 		return (<div className="sm-flex flex-wrap mxn2">
-			<div className="col col-6 mx-auto p2">
+			<div className="col col-12 sm-col-6 mx-auto p2">
 				<Helmet
   			title="About | Staytube"
   			meta={[{ property: 'og:title', content: 'About' }]}
@@ -19,6 +19,8 @@ class About extends React.Component {
 					<li>PostCSS</li>
 					<li>Basscss</li>
 					<li>YouTube API</li>
+					<li>Surge</li>
+					<li>Cloudflare</li>
 				</ul>
 				</div>
 			</div>);
