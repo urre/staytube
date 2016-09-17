2be48d62311c6fbf6e752cf2be5ff05ecbf57fb7
Urban Sanden <hej@urre.me>
2016-09-17 02:08:51 +0200

    Add footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Footer = () => {
 	return (
-			<div className="sm-flex flex-wrap mxn2">
-				<div className="sm-col-12 p2 center">
-					<p><small><a href="https://twitter.com/urre">Suggest a concert</a></small></p>
+		<footer className="footer">
+			<div className="container">
+				<div className="sm-flex flex-wrap mxn2">
+					<div className="col col-12 sm-col-6 mx-auto p2">
+						<nav role="navigation">
+							<ul className="list-reset flex justify-center">
+								<li className="mr3"><a href="https://twitter.com/urre">Suggest a concert?</a></li>
+								<li className="mr3"><Link to="/">Concerts</Link></li>
+								<li className="mr3"><Link to="/about">About</Link></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
 			</div>
-			</div>
+		</footer>
 	);
 };
 
